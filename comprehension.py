7c145a64bf292bb141c73da9738783ad6b411f67
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-30 14:55:34 +0530

    New file comprehension.py is added

---
+++ comprehension.py
@@ -0,0 +1,20 @@
+comb=[]
+for x in[1,2,3]:
+    for y in [3,1,4]:
+        if x !=y:
+            comb.append((x,y))
+print(comb)
+
+
+import sys
+sys.exit()
+squares = list(map(lambda i:i*i,range(1,11)))
+print(squares)
+
+import sys
+sys.exit()
+
+squares=[]
+for i in range(1,10):
+    squares.append(i*i)
+print(squares)
